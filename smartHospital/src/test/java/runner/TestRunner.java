6ffe629baa81e6ffd3669de194c82a6605acd5ad
Tabbusree Sridhar <tabbusree.sridhar@expleogroup.com>
2023-11-27 16:13:53 +0530

    converted to tetng

--- smartHospital/src/test/java/runner/TestRunner.java
+++ smartHospital/src/test/java/runner/TestRunner.java
@@ -1,16 +1,15 @@
 package runner;
 
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
 //testng
 //import io.cucumber.testng.AbstractTestNGCucumberTests;
 //import io.cucumber.testng.CucumberOptions;
  
-@RunWith(Cucumber.class)
 @CucumberOptions(tags = "",features="src/test/resources/Features",glue ="com.stepdefinition",plugin= {"json:target/cucumber-report.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
-public class TestRunner    {
+public class TestRunner  extends AbstractTestNGCucumberTests {
 //	@Override
 //	extends AbstractTestNGCucumberTests
 //	@DataProvider(parallel=true)
